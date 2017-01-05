745702550999f1a6fffd71ad5d02e40c447e779f
Dylan Keyer <keyer42@gmail.com>
2017-01-04 19:21:47 -0500

    forms

---
+++ quote/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from .models import Quote
+from django import models
+
+class QuoteForm(models.ModelForm):
+	pass
--- quote/models.py
+++ quote/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Quote(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    carrier = models.CharField(max_length=30)
+    quote = models.IntegerField()
+    condition = models.CharField(max_length=8)
+    storage = models.IntegerField()
+
+    def __str__(self):  #For Python 2, use __str__ on Python 3
+        return self.name
